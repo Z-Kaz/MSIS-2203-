f1d73e6b4a728241b1541131f6eee336a0e43194
Ben Welch <49293460+Z-Kaz@users.noreply.github.com>
2019-05-11 00:18:15 -0500

    Add files via upload

---
+++ Homework_11/Creature.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_11
+{
+    class Creature
+    {
+        protected static Random num = new Random();
+        public string Name { get; set; }
+        public string CreatureType { get; set; }
+        public int BaseAttack = 10;
+        public int BaseHealth = 50;
+
+        public int CreatureAttack { get; set; }
+        public int CreatureHealth { get; set; }
+        public int humanAttack;
+
+        public Creature(string Name, string CreatureType)
+        {
+            this.Name = Name;
+            this.CreatureType = CreatureType;
+            this.CreatureHealth = BaseHealth;
+            this.CreatureAttack = BaseAttack;
+        }
+        public Creature() { }
+
+        public virtual int Attack()
+        {
+            humanAttack = BaseAttack + num.Next(-5, 6);
+            if (humanAttack < 0)
+            {
+                humanAttack = 0;
+            }
+            return humanAttack;
+        }
+    }
+}
---
+++ Homework_11/Orc.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework_11
+{
+    class Orc : Creature
+    {
+        public int orcAttack;
+
+        public Orc(string name)
+        {
+            this.Name = name;
+            this.CreatureType = "Orc";
+            this.CreatureAttack = BaseAttack + num.Next(5, 11);
+            this.CreatureHealth = BaseHealth + num.Next(5, 26);
+        }
+
+        public override int Attack()
+        {
+            int RageValue = num.Next(0, 11);
+            orcAttack = CreatureAttack + RageValue;
+            CreatureHealth -= RageValue;
+            if(orcAttack < 0)
+            {
+                orcAttack = 0;
+            }
+            return orcAttack;
+        }
+    }
+}
